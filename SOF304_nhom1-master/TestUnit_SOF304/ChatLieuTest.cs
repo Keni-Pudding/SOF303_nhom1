0e63263b1d5a9c08df35e112d620ae8a55090873
Duy laptop <ASUS@LAPTOP-8QJQGJTD>
2024-02-20 09:39:20 +0700

    Duy's Test

---
+++ SOF304_nhom1-master/TestUnit_SOF304/ChatLieuTest.cs
@@ -0,0 +1,269 @@
+﻿using CTN4_Data.DB_Context;
+using CTN4_Data.Models.DB_CTN4;
+using CTN4_Serv.Service;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace TestUnit_SOF304
+{
+    [TestClass]
+    public class ChatLieuTest
+    {
+        ChatLieu _chatlieu;
+        DB_CTN4_ok _dbcontext;
+        ChatLieuService _chatlieusv;
+
+        public ChatLieuTest()
+        {
+            _chatlieu = new ChatLieu();
+            _dbcontext = new DB_CTN4_ok();
+            _chatlieusv = new ChatLieuService();
+        }
+
+        [Test]
+        public void ACl_01()
+        {
+            ChatLieu chatlieu = new ChatLieu() { Id = new Guid(), TenChatLieu = "", TrangThai = true, Is_detele = true };
+
+            var result = _chatlieusv.Them(chatlieu);
+
+            _chatlieu = _dbcontext.ChatLieus.Find(chatlieu.Id);
+            Assert.AreEqual("", _chatlieu.TenChatLieu);
+        }
+
+        [Test]
+        public void ACL_02()
+        {
+            ChatLieu chatlieu = new ChatLieu() { Id = new Guid(), TenChatLieu = "Da Báo", TrangThai = true, Is_detele = true };
+
+            var result = _chatlieusv.Them(chatlieu);
+
+            _chatlieu = _dbcontext.ChatLieus.Find(chatlieu.Id);
+            Assert.AreEqual("Da Báo", _chatlieu.TenChatLieu);
+        }
+
+        [Test]
+        public void ACl_03()
+        {
+            ChatLieu chatlieu = new ChatLieu() { Id = new Guid(), TenChatLieu = "123456789012345678901234567890", TrangThai = true, Is_detele = true };
+
+            var result = _chatlieusv.Them(chatlieu);
+
+            Assert.IsTrue(result);
+        }
+        [Test]
+        public void ACl_04()
+        {
+            ChatLieu chatlieu = new ChatLieu() { Id = new Guid(), TenChatLieu = "Dadadadadadadadadadadaadadadadaaaad", TrangThai = true, Is_detele = true };
+
+            var result = _chatlieusv.Them(chatlieu);
+
+            Assert.IsFalse(result);
+        }
+        [Test]
+        public void ACl_05()
+        {
+            ChatLieu chatlieu = new ChatLieu() { Id = new Guid(), TenChatLieu = "!@#$%^", TrangThai = true, Is_detele = true };
+
+            var result = _chatlieusv.Them(chatlieu);
+
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void ACl_06()
+        {
+            ChatLieu chatlieu = new ChatLieu() { Id = new Guid(), TenChatLieu = new string('a', 100), TrangThai = true, Is_detele = true };
+
+            var result = _chatlieusv.Them(chatlieu);
+
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void ACl_07()
+        {
+            ChatLieu chatlieu = new ChatLieu() { Id = new Guid(), TenChatLieu = "Dada", TrangThai = true, Is_detele = true };
+
+            var result = _chatlieusv.Them(chatlieu);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        [DataRow("420FC0D1-12C3-44AC-A1C7-08DC3157C185")]
+        public void UCL_01(string idstring)
+        {
+            Guid id = new Guid(idstring);
+            ChatLieu chatlieu = _dbcontext.ChatLieus.Find(id);
+
+            if (chatlieu != null)
+            {
+                string newTenChatLieu = "Da";
+                chatlieu.TenChatLieu = newTenChatLieu;
+
+                _chatlieusv.Sua(chatlieu);
+                _chatlieu = _dbcontext.ChatLieus.Find(chatlieu.Id);
+
+                Assert.AreEqual(newTenChatLieu, _chatlieu.TenChatLieu);
+            }
+            else
+            {
+                Assert.Fail("ID không tồn tại");
+            }
+        }
+
+        [TestMethod]
+        [DataRow("420FC0D1-12C3-44AC-A1C7-08DC3157C185")]
+        public void UCL_02(string idstring)
+        {
+            Guid id = new Guid(idstring);
+            ChatLieu chatlieu = _dbcontext.ChatLieus.Find(id);
+
+            if (chatlieu != null)
+            {
+                string newTenChatLieu = "Da báo";
+                chatlieu.TenChatLieu = newTenChatLieu;
+
+                var result = _chatlieusv.Sua(chatlieu);
+                _chatlieu = _dbcontext.ChatLieus.Find(chatlieu.Id);
+
+                Assert.IsFalse(result);
+            }
+            else
+            {
+                Assert.Fail("ID không tồn tại");
+            }
+        }
+
+        [TestMethod]
+        [DataRow("420FC0D1-12C3-44AC-A1C7-08DC3157C185")]
+        public void UCL_03(string idstring)
+        {
+            Guid id = new Guid(idstring);
+            ChatLieu chatlieu = _dbcontext.ChatLieus.Find(id);
+
+            if (chatlieu != null)
+            {
+                string newTenChatLieu = null;
+                bool newTrangThai = false;
+                bool newIs_delete = false;
+                chatlieu.TenChatLieu = newTenChatLieu;
+
+                var result = _chatlieusv.Sua(chatlieu);
+                _chatlieu = _dbcontext.ChatLieus.Find(chatlieu.Id);
+
+                Assert.IsFalse(result);
+            }
+            else
+            {
+                Assert.Fail("ID không tồn tại");
+            }
+        }
+
+        [TestMethod]
+        [DataRow("420FC0D1-12C3-44AC-A1C7-08DC3157C185")]
+        public void UCL_04(string idstring)
+        {
+            Guid id = new Guid(idstring);
+            ChatLieu chatlieu = _dbcontext.ChatLieus.Find(id);
+
+            if (chatlieu != null)
+            {
+                string newTenChatLieu = "Da Báo";
+                string newGhiChu = "ok";
+                bool newTrangThai = true;
+                bool newIs_delete = true;
+
+                chatlieu.TenChatLieu = newTenChatLieu;
+                chatlieu.GhiChu = newGhiChu;
+                chatlieu.TrangThai = newTrangThai;
+                chatlieu.Is_detele = newIs_delete;
+
+                var result = _chatlieusv.Sua(chatlieu);
+                _chatlieu = _dbcontext.ChatLieus.Find(chatlieu.Id);
+
+                Assert.IsTrue(result);
+            }
+            else
+            {
+                Assert.Fail("ID không tồn tại");
+            }
+        }
+
+        [Test]
+        public void GACL_01()
+        {
+            var clservice = new ChatLieuService();
+
+            var actualcl = clservice.GetAll();
+
+            Assert.IsNotNull(actualcl);
+            Assert.IsInstanceOfType<List<ChatLieu>>(actualcl); //Kiểm tra xem actualSizes có nằm trong List<Size> hay không. Nếu không ném ra Exception
+        }
+
+        [TestMethod]
+        [DataRow("99D20DCB-B01B-430F-CC94-08DC315803FF")]
+        public void DECL_01(string idstring)
+        {
+            Guid id = new Guid(idstring);
+            ChatLieu chatlieu = _dbcontext.ChatLieus.Find(id);
+
+            if (chatlieu != null)
+            {
+                var result = _chatlieusv.Xoa(id);
+
+                Assert.IsTrue(result);
+            }
+            else
+            {
+                Assert.Fail("ID không tồn tại hoặc đã bị xóa");
+            }
+        }
+
+        [Test]
+        public void SCL_01()
+        {
+            Guid IDnull = Guid.Empty;
+
+            var result = _chatlieusv.GetById(IDnull);
+
+            Assert.IsNull(result);
+        }
+
+        [Test]
+        public void SS_02()
+        {
+            Guid id = Guid.Parse("B58665CA-E7E6-4B6A-9CBC-08DC315874CF");
+
+            var result = _chatlieusv.GetById(id);
+
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void SS_03()
+        {
+            Guid id = Guid.Parse("A58665CA-E7E6-4B6A-9CBC-08DC315874CF");
+
+            var result = _chatlieusv.GetById(id);
+
+            Assert.IsNull(result);
+        }
+
+        [Test]
+        public void SS_04()
+        {
+            Guid id = Guid.Parse("@#$%^&");
+
+            var result = _chatlieusv.GetById(id);
+
+            Assert.IsNull(result, "Guid không được chứa ký tự đặc biệt");
+        }
+    }
+}
--- SOF304_nhom1-master/TestUnit_SOF304/SizeTest.cs
+++ SOF304_nhom1-master/TestUnit_SOF304/SizeTest.cs
@@ -8,6 +8,8 @@ using System.Threading.Tasks;
 using CTN4_Data.DB_Context;
 using CTN4_Data.Models.DB_CTN4;
 using CTN4_Serv.Service;
+using CTN4_View_Admin.Controllers.QuanLY;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 using Size = CTN4_Data.Models.DB_CTN4.Size;
@@ -72,13 +74,13 @@ namespace TestUnit_SOF304
             Assert.IsFalse(result, "Không được thêm vào database khi Tên Size hoặc Cỡ size đã tồn tại");
         }
 
-        [Test]
+        [Test] 
         public void AS_05()
         {
-            Size size = new Size() { Id = new Guid(), TenSize = new string('a',30), CoSize = "XL", TrangThai = true, Is_detele = true };
+            Size size = new Size() { Id = new Guid(), TenSize = new string('a', 30), CoSize = "XL", TrangThai = true, Is_detele = true };
 
-           var result =_sizesv.Them(size);
-           
+            var result = _sizesv.Them(size);
+
             Assert.IsTrue(result);
         }
 
@@ -92,6 +94,61 @@ namespace TestUnit_SOF304
             Assert.IsFalse(result, "Không được thêm vào db khi Tên Size dài quá 30 ký tự");
         }
 
+        [Test]
+        public void AS_07()
+        {
+            Size size = new Size() { Id = new Guid(), TenSize = "15cm x 9.5cm x 8cm", CoSize = null, TrangThai = true, Is_detele = true };
+
+            var result = _sizesv.Them(size);
+
+            _size = _dbcontext.Sizes.Find(size.Id);
+            Assert.IsFalse(result, "Không được thêm vào db khi Cỡ Size trống");
+            /*Assert.AreEqual("15cm x 9.5cm x 8cm", _size.TenSize);
+            Assert.AreEqual(null, _size.CoSize);*/
+        }
+        [Test]
+        public void AS_08()
+        {
+            Size size = new Size() { Id = new Guid(), TenSize = "15cm x 9.5cm x 8cm", CoSize = "123123", TrangThai = true, Is_detele = true };
+
+            var result = _sizesv.Them(size);
+
+            _size = _dbcontext.Sizes.Find(size.Id);
+            Assert.IsFalse(result, "Chỉ được nhập chữ");
+        }
+
+        [Test]
+        public void AS_09()
+        {
+            Size size = new Size() { Id = new Guid(), TenSize = "15cm x 9.5cm x 7cm", CoSize = new string('S', 31), TrangThai = true, Is_detele = true };
+
+            var result = _sizesv.Them(size);
+
+            Assert.IsFalse(result, "Không được thêm vào db khi Cỡ Size dài quá 30 ký tự");
+        }
+
+        [Test]
+        public void AS_10()
+        {
+
+        }
+
+        [Test]
+        public void AS_11()
+        {
+            Size size = new Size()
+            {
+                Id = new Guid(),
+                TenSize = "@#$%",
+                CoSize = "@#$%",
+                TrangThai = true,
+                Is_detele = true
+            };
+
+            var result = _sizesv.Them(size);
+
+            Assert.IsFalse(result, "Chỉ được nhập chữ");
+        }
 
         [TestMethod]
         [DataRow("56DD3EE2-C4DF-4376-B982-E2C0F7081149")]
@@ -117,6 +174,267 @@ namespace TestUnit_SOF304
             {
                 Assert.Fail("ID không tồn tại");
             }
+        }
+
+        [TestMethod]
+        [DataRow("56DD3EE2-C4DF-4376-B982-E2C0F7081149")]
+        public void US_02(string idstring)
+        {
+            Guid id = new Guid(idstring);
+            Size size = _dbcontext.Sizes.Find(id);
+
+            if (size != null)
+            {
+                string newTenSize = "15cm x 9.5cm x 15cm";
+                string newCoSize = "SL";
+                bool newTrangThai = true;
+                bool newIs_detele = true;
+                size.TenSize = newTenSize;
+                size.CoSize = newCoSize;
+                size.TrangThai = newTrangThai;
+                size.Is_detele = newIs_detele;
+
+                _sizesv.Sua(size);
+                _size = _dbcontext.Sizes.Find(size.Id);
+
+                Assert.AreEqual(newTenSize, _size.TenSize);
+                Assert.AreEqual(newCoSize, _size.CoSize);
+                Assert.AreEqual(newTrangThai, _size.TrangThai);
+                Assert.AreEqual(newIs_detele, _size.Is_detele);
+            }
+            else
+            {
+                Assert.Fail("ID không tồn tại");
+            }
+        }
+
+        [TestMethod]
+        [DataRow("56DD3EE2-C4DF-4376-B982-E2C0F7081149")]
+        public void US_03(string idstring)
+        {
+            Guid id = new Guid(idstring);
+            Size size = _dbcontext.Sizes.Find(id);
+
+            if (size != null)
+            {
+                string newTenSize = "15cm x 9.5cm x 15cm";
+                string newCoSize = "SL";
+                bool newTrangThai = false;
+                bool newIs_detele = false;
+                size.TenSize = newTenSize;
+                size.CoSize = newCoSize;
+                size.TrangThai = newTrangThai;
+                size.Is_detele = newIs_detele;
+
+                _sizesv.Sua(size);
+                _size = _dbcontext.Sizes.Find(size.Id);
+
+                Assert.AreEqual(newTenSize, _size.TenSize);
+                Assert.AreEqual(newCoSize, _size.CoSize);
+                Assert.AreEqual(newTrangThai, _size.TrangThai);
+                Assert.AreEqual(newIs_detele, _size.Is_detele);
+            }
+            else
+            {
+                Assert.Fail("ID không tồn tại");
+            }
+        }
+
+        [TestMethod]
+        [DataRow("56DD3EE2-C4DF-4376-B982-E2C0F7081149")]
+        public void US_04(string idstring)
+        {
+            Guid id = new Guid(idstring);
+            Size size = _dbcontext.Sizes.Find(id);
+
+            if (size != null)
+            {
+                string newTenSize = "15cm x 9.5cm x 15cm";
+                string newCoSize = null;
+                bool newTrangThai = true;
+                bool newIs_detele = true;
+                size.TenSize = newTenSize;
+                size.CoSize = newCoSize;
+                size.TrangThai = newTrangThai;
+                size.Is_detele = newIs_detele;
+
+                var result = _sizesv.Sua(size);
+                _size = _dbcontext.Sizes.Find(size.Id);
+
+                Assert.IsFalse(result, "Không thêm được do trống trường Cỡ Size");
+            }
+            else
+            {
+                Assert.Fail("ID không tồn tại");
+            }
+        }
+
+
+        [TestMethod]
+        [DataRow("56DD3EE2-C4DF-4376-B982-E2C0F7081149")]
+        public void US_05(string idstring)
+        {
+            Guid id = new Guid(idstring);
+            Size size = _dbcontext.Sizes.Find(id);
+
+            if (size != null)
+            {
+                string newTenSize = null;
+                string newCoSize = "S";
+                bool newTrangThai = true;
+                bool newIs_detele = true;
+                size.TenSize = newTenSize;
+                size.CoSize = newCoSize;
+                size.TrangThai = newTrangThai;
+                size.Is_detele = newIs_detele;
+
+                var result = _sizesv.Sua(size);
+                _size = _dbcontext.Sizes.Find(size.Id);
+
+                Assert.IsFalse(result, "Không thêm được do trống trường Cỡ Size");
+            }
+            else
+            {
+                Assert.Fail("ID không tồn tại");
+            }
+        }
+
+        [TestMethod]
+        [DataRow("56DD3EE2-C4DF-4376-B982-E2C0F7081149")]
+        public void US_06(string idstring)
+        {
+            Guid id = new Guid(idstring);
+            Size size = _dbcontext.Sizes.Find(id);
+
+            if (size != null)
+            {
+                string newTenSize = "15cm x 9.5cm x 7cmx15cm x 9.5cm x 7cmx1";
+                string newCoSize = "S";
+                bool newTrangThai = true;
+                bool newIs_detele = true;
+                size.TenSize = newTenSize;
+                size.CoSize = newCoSize;
+                size.TrangThai = newTrangThai;
+                size.Is_detele = newIs_detele;
+
+                var result = _sizesv.Sua(size);
+                _size = _dbcontext.Sizes.Find(size.Id);
+
+                Assert.IsFalse(result, "Không được thêm dữ liệu khi quá 30 ký tự");
+            }
+            else
+            {
+                Assert.Fail("ID không tồn tại");
+            }
+        }
+
+        [TestMethod]
+        [DataRow("56DD3EE2-C4DF-4376-B982-E2C0F7081149")]
+        public void US_07(string idstring)
+        {
+            Guid id = new Guid(idstring);
+            Size size = _dbcontext.Sizes.Find(id);
+
+            if (size != null)
+            {
+                string newTenSize = "15cm x 9.5cm x 7cm";
+                string newCoSize = "SSSSSSSSSSSSSSSSSSSSSSSSSSSS";
+                bool newTrangThai = true;
+                bool newIs_detele = true;
+                size.TenSize = newTenSize;
+                size.CoSize = newCoSize;
+                size.TrangThai = newTrangThai;
+                size.Is_detele = newIs_detele;
+
+                var result = _sizesv.Sua(size);
+                _size = _dbcontext.Sizes.Find(size.Id);
+
+                Assert.IsTrue(result, "Không được thêm dữ liệu khi quá 30 ký tự");
+            }
+            else
+            {
+                Assert.Fail("ID không tồn tại");
+            }
+        }
+
+        [Test]
+        public void GAS_01()
+        {
+            var sizeService = new SizeService();
+
+            var actualSizes = sizeService.GetAll();
+
+            Assert.IsNotNull(actualSizes);
+            Assert.IsInstanceOfType<List<Size>>(actualSizes); //Kiểm tra xem actualSizes có nằm trong List<Size hay không. Nếu không ném ra Exception
+        }
+
+        [TestMethod]
+        [DataRow("8181B4CE-517F-46F7-13E1-08DC30602A6D")]
+        public void DS_01(string idstring)
+        {
+            Guid id = new Guid(idstring);
+            Size size = _dbcontext.Sizes.Find(id);
+
+            if (size != null)
+            {
+                var result = _sizesv.Xoa(id);
+
+                Assert.IsTrue(result);
+            }
+            else
+            {
+                Assert.Fail("ID không tồn tại hoặc đã bị xóa");
+            }
+        }
+
+        [Test]
+        public void SS_01()
+        {
+            Guid IDnull = Guid.Empty;
+
+            var result = _sizesv.GetById(IDnull);
+
+            Assert.IsNull(result);
+        }
+
+        [Test]
+        public void SS_02()
+        {
+            Guid id = Guid.Parse("4B92378D-06D7-452C-FAE6-08DC2460F570");
+
+            var result = _sizesv.GetById(id);
+
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void SS_03()
+        {
+            Guid id = Guid.Parse("1292378D-06D7-452C-FAE6-08DC2460F570");
+
+            var result = _sizesv.GetById(id);
+
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void SS_04()
+        {
+            Guid id = Guid.Parse("1292378D-06D7-452C-FAE6-08DC2460F570");
+
+            var result = _sizesv.GetById(id);
+
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void SS_05()
+        {
+            // Arrange
+            string IDfail = "@#$%";
+
+            // Act & Assert
+            Assert.ThrowsException<ArgumentException>(() => _sizesv.GetById(Guid.Parse(IDfail)));
         }
     }
 }
